13512958f174fe3f767935acffd2c98b0fbf91a8
Akshay Waghmare <akshay.waghmare@meganexus.com>
2019-04-10 16:27:00 +0530

    changes

--- src/main/java/com/samples/learning/tail/Tail.java
+++ src/main/java/com/samples/learning/tail/Tail.java
@@ -1,19 +1,21 @@
 package com.samples.learning.tail;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Tail implements LogFileTailerListener {
 	  /**
 	   * The log file tailer
 	   */
 	  private LogFileTailer tailer;
-
+	  HashMap<Integer,Integer> codeCount = new HashMap<>();
 	  /**
 	   * Creates a new Tail instance to follow the specified file
 	   */
 	  public Tail( String filename )
 	  {
-	    tailer = new LogFileTailer( new File( filename ), 1000, false );
+	    tailer = new LogFileTailer( new File( filename ), 1000, true );
 	    tailer.addLogFileTailerListener( this );
 	    tailer.start();
 	  }
@@ -25,8 +27,34 @@ public class Tail implements LogFileTailerListener {
 	   */
 	  public void newLogFileLine(String line)
 	  {
+		
+		Integer prevCount = 0;
 	    System.out.println( line );
+	    String[] code =  alltokens(line);
+	    System.out.println(code[5]);
+	    if(!codeCount.containsKey(Integer.parseInt(code[5]))) {
+	    	codeCount.put(Integer.parseInt(code[5]), 1);
+	    }
+	    else {
+	    	prevCount = codeCount.get(Integer.parseInt(code[5]));
+	    	codeCount.put(Integer.parseInt(code[5]), prevCount+1);
+	    }
+	    for (Entry<Integer, Integer> entry : codeCount.entrySet()) {
+			System.out.println("code" + entry.getKey());
+			System.out.println("val" + entry.getValue());
+		}
+	   
 	  }
+
+	public String[] alltokens(String line) {
+//		StringTokenizer str = new StringTokenizer(line, " ");
+//	    str.countTokens();
+//	    while(str.hasMoreTokens()) {
+//	    	System.out.println(str.nextToken()+" "+str.nextElement());
+//	    }
+		String[] s = line.split("\\s+");
+		return s;
+	}
 
 	  /**
 	   * Command-line launcher
